294be92cabe85700b1dd0438b0a9138e7d4e9f97
Diego Ritzel <dipi.evil@gmail.com>
2022-06-04 12:01:43 -0300

    Add second screen

---
+++ app/src/main/java/br/dipievil/helloworld/Tela02Activity.java
@@ -0,0 +1,14 @@
+package br.dipievil.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela02);
+    }
+}
