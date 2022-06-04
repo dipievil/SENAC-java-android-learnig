2649f68fa6cc5c216e2908c88ac092ea25351bbe
Diego Ritzel <dipi.evil@gmail.com>
2022-06-04 13:58:11 -0300

    Remove unnecessary persistance

--- app/src/main/java/br/dipievil/helloworld/Tela01Activity.java
+++ app/src/main/java/br/dipievil/helloworld/Tela01Activity.java
@@ -1,7 +1,6 @@
 package br.dipievil.helloworld;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Tela01Activity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_tela01);
     }
